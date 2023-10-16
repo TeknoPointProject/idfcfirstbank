2419242baaa7a7a9555158892272be758bf4078f
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-10-16 18:49:08 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,11 +1,25 @@
-export default function decorate(block)  {
+export default function decorate(block) {
   const buttons = document.createElement('div');
   buttons.className = 'carousel-buttons';
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, i) => {
+      if (entry.isIntersecting) {
+        const image = entry.target;
+        image.setAttribute('loading', 'eager'); // Preload when in viewport
+        if (!i) image.setAttribute('loading', 'auto'); // Load the first image in the viewport eagerly
+        observer.unobserve(image);
+      }
+    });
+  });
+
   [...block.children].forEach((row, i) => {
     const classes = ['image', 'text'];
     classes.forEach((e, j) => {
-      row.children[j].classList.add(`carousel-${e}`);
+      const image = row.children[j];
+      image.classList.add(`carousel-${e}`);
+      observer.observe(image); // Observe each image
     });
+
     /* buttons */
     const button = document.createElement('button');
     button.title = 'Carousel Nav';
@@ -17,5 +31,6 @@ export default function decorate(block)  {
     });
     buttons.append(button);
   });
+
   block.parentElement.append(buttons);
 }
