881ba800e390abf44908658d372da0a04a0f7c4e
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-10-16 18:26:43 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,6 +1,17 @@
-export default function decorate(block)  {
+export default function decorate(block) {
   const buttons = document.createElement('div');
   buttons.className = 'carousel-buttons';
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry, i) => {
+      if (entry.isIntersecting) {
+        const image = entry.target.querySelector('img');
+        image.loading = 'auto'; // Load the image immediately
+        buttons.children[i].classList.add('selected');
+        observer.unobserve(entry.target);
+      }
+    });
+  });
+
   [...block.children].forEach((row, i) => {
     const classes = ['image', 'text'];
     classes.forEach((e, j) => {
@@ -9,7 +20,13 @@ export default function decorate(block)  {
     /* buttons */
     const button = document.createElement('button');
     button.title = 'Carousel Nav';
-    if (!i) button.classList.add('selected');
+    if (!i) {
+      button.classList.add('selected');
+      // Load the image immediately if it's in the first fold
+      row.querySelector('img').loading = 'auto';
+    } else {
+      observer.observe(row);
+    }
     button.addEventListener('click', () => {
       block.scrollTo({ top: 0, left: row.offsetLeft - row.parentNode.offsetLeft, behavior: 'smooth' });
       [...buttons.children].forEach((r) => r.classList.remove('selected'));
