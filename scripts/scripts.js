5b617663413e5aff2884785f4a28f3f406dc4ff4
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-10-17 16:08:47 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -108,7 +108,7 @@ async function loadLazy(doc) {
   loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  // loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   // loadFonts();
 
   sampleRUM('lazy');
