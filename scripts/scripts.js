4f86fd093c36c221a1cf51d5ab0b546b110f341f
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-12-08 12:52:47 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -152,11 +152,14 @@ if (sk) {
 }
 
 
- function logMovies() {
-  const response =  fetch("https://main--idfcfirstbank--teknopointproject.hlx.page/data.json");
-  const movies =  response.json();
-  console.log(movies);
-}
-
-logMovies()
+ // API for get requests
+let fetchRes = fetch(
+"https://jsonplaceholder.typicode.com/todos/1");
+     
+// FetchRes is the promise to resolve
+// it by using.then() method
+fetchRes.then(res =>
+    res.json()).then(d => {
+        console.log(d)
+    })
 
