9336008ab40dee6224a037a930c83357f6193035
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-12-08 13:48:42 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,58 +1,32 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-
-export default function decorate(block) {
-  /* change to ul, li */
-  const ul = document.createElement('ul');
-  [...block.children].forEach((row) => {
-    const li = document.createElement('li');
-    while (row.firstElementChild) li.append(row.firstElementChild);
-    [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
-    });
-    ul.append(li);
-  });
-  ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
-  block.textContent = '';
-  block.append(ul);
-}
-const proxyUrl = 'https://api.allorigins.win/raw?url=';
-const apiUrl = 'https://main--idfcfirstbank--teknopointproject.hlx.live/data.json';
-
-
-
-
 fetch(proxyUrl + encodeURIComponent(apiUrl))
   .then(response => response.json())
   .then(data => {
     var div = document.createElement('div');
     div.classList.add("apidata");
 
-    // Loop through each object in the array
-    data.forEach(item => {
-      // Create a new div for each item
-      var itemDiv = document.createElement('div');
+    // Check if data is an array
+    if (Array.isArray(data)) {
+      data.forEach(item => {
+        var itemDiv = document.createElement('div');
 
-      // Create spans for Source and Destination properties
-      var sourceSpan = document.createElement('span');
-      sourceSpan.textContent = item.Source + ": ";
+        var sourceSpan = document.createElement('span');
+        sourceSpan.textContent = item.Source + ": ";
 
-      var destinationSpan = document.createElement('span');
-      destinationSpan.textContent = item.Destination;
+        var destinationSpan = document.createElement('span');
+        destinationSpan.textContent = item.Destination;
 
-      // Append spans to the item div
-      itemDiv.appendChild(sourceSpan);
-      itemDiv.appendChild(destinationSpan);
+        itemDiv.appendChild(sourceSpan);
+        itemDiv.appendChild(destinationSpan);
 
-      // Append item div to the main div
-      div.appendChild(itemDiv);
-    });
+        div.appendChild(itemDiv);
+      });
 
-    // Append the main div to the body
-    var body = document.body;
-    body.appendChild(div);
+      var body = document.body;
+      body.appendChild(div);
+    } else {
+      console.error('Error: Data is not an array');
+    }
   })
   .catch(error => {
     console.error('Error:', error);
   });
-
