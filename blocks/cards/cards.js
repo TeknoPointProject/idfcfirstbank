5240f74310370a3c9c0f6fb227953b637445f211
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2023-12-08 13:52:19 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -25,33 +25,11 @@ const apiUrl = 'https://main--idfcfirstbank--teknopointproject.hlx.live/data.jso
 fetch(proxyUrl + encodeURIComponent(apiUrl))
   .then(response => response.json())
   .then(data => {
-    var div = document.createElement('div');
-    div.classList.add("apidata");
-
-    // Check if data is an array
-    if (Array.isArray(data)) {
-      data.forEach(item => {
-        var itemDiv = document.createElement('div');
-
-        var sourceSpan = document.createElement('span');
-        sourceSpan.textContent = item.Source + ": ";
-
-        var destinationSpan = document.createElement('span');
-        destinationSpan.textContent = item.Destination;
-
-        itemDiv.appendChild(sourceSpan);
-        itemDiv.appendChild(destinationSpan);
-
-        div.appendChild(itemDiv);
-      });
-
-      var body = document.body;
-      body.appendChild(div);
-    } else {
-      console.error('Error: Data is not an array');
-    }
+    console.log(typeof data);
+    console.log(data);
   })
   .catch(error => {
     console.error('Error:', error);
   });
+
 
